aa485dbfce646f06cef13683e835c5776cbc8cf5
Chris-Bitler <crb2547@rit.edu>
2018-08-16 19:25:38 -0400

    Update navbar

--- admin.php
+++ admin.php
@@ -26,6 +26,9 @@ if (!$_SESSION['id']) {
     <nav>
         <div class="nav-wrapper foodtrax-pink">
             <a href="#" class="brand-logo center">FoodTrax</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="/">Home</a></li>
+            </ul>
         </div>
         <div class="container">
             <div class="center">
