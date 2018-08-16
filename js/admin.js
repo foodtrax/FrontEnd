2700fcc1373b3bf5ffe5dd7dfaad3e47beb8a302
Chris-Bitler <crb2547@rit.edu>
2018-08-16 18:16:41 -0400

    Admin updates

--- js/admin.js
+++ js/admin.js
@@ -23,7 +23,15 @@ function getCollapsibleBody(truck) {
     contents += '<input id="twitter-' + truck.id + '" type="text" class="underline-input" value="' + truck.twitter + '">';
     contents += '<label for="twitter-' + truck.id + '">Twitter</label>';
     contents += '</div>';
-    contents += '<button class="btn waves-effect waves-light" type="button" id="updateButton-' + truck.id  + '">Update</button>';
+    contents += '<div class="input-field col s12">';
+    contents += '<input id="facebook-' + truck.id + '" type="text" class="underline-input" value="' + truck.facebook + '">';
+    contents += '<label for="facebook-' + truck.id + '">Facebook</label>';
+    contents += '</div>';
+    contents += '<div class="input-field col s12">';
+    contents += '<input id="website-' + truck.id + '" type="text" class="underline-input" value="' + truck.website + '">';
+    contents += '<label for="website-' + truck.id + '">Website</label>';
+    contents += '</div>';
+    contents += '<button class="btn waves-effect waves-light" type="button" data-truck-id="' + truck.id + '" id="updateButton-' + truck.id  + '">Update</button>';
     contents += '<button class="btn waves-effect waves-light" type="button" id="updateLocButton-' + truck.id  + '">Update Truck Location</button>';
     contents += '<button class="btn waves-effect waves-light" type="button" id="hideButton-' + truck.id  + '">Hide Truck from Map</button>';
     contents += '</div>';
@@ -31,6 +39,95 @@ function getCollapsibleBody(truck) {
 
     return contents;
 }
+
+function updateTruckInformation(id)
+{
+    var name = $("#name-"  + id).val();
+    var description = $("#description-" + id).val();
+    var twitter = $("#twitter-" + id).val();
+    var facebook = $("#facebook-" + id).val();
+    var website = $("#website-" + id).val();
+
+    $.post(
+        'https://roc.foodtrax.io/backend/api/update_truck_web.php',
+        {
+            truckid: id,
+            truckname: name,
+            truckdesc: description,
+            twitter: twitter,
+            facebook: facebook,
+            website: website
+        },
+        function(data) {
+            var json = JSON.parse(data);
+            if(json.result === true) {
+                $("#notification-" + id).html('Truck information updated.');
+                $("#notification-" + id).removeClass('notification-bad');
+                $("#notification-" + id).addClass('notification-ok');
+            } else {
+                $("#notification-" + id).html('An error occurred during update.');
+                $("#notification-" + id).removeClass('notification-ok');
+                $("#notification-" + id).addClass('notification-bad');
+            }
+        }
+    )
+}
+
+function updateTruckLocation(id)
+{
+    if(navigator.geolocation) {
+        var loc = navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+        $("#notification-" + id).addClass('notification-bad');
+        $("#notification-" + id).removeClass('notification-ok');
+        $("#notification-" + id).html("Couldn't get location");
+        return;
+    }
+
+    $.post(
+        'https://roc.foodtrax.io/backend/api/update_truck_loc_web.php',
+        {
+            truckId: id,
+            lat: loc.coords.latitude,
+            lon: loc.coords.longitude
+        },
+        function(data) {
+            var json = JSON.parse(data);
+            if(json.result === true) {
+                $("#notification-" + id).html('Truck location updated.');
+                $("#notification-" + id).removeClass('notification-bad');
+                $("#notification-" + id).addClass('notification-ok');
+            } else {
+                $("#notification-" + id).html('An error occurred during update.');
+                $("#notification-" + id).removeClass('notification-ok');
+                $("#notification-" + id).addClass('notification-bad');
+            }
+        }
+    )
+}
+
+function hideTruckLocation(id)
+{
+    $.post(
+        'https://roc.foodtrax.io/backend/api/hide_truck_web.php',
+        {
+            truckId: id,
+        },
+        function(data) {
+            var json = JSON.parse(data);
+            if(json.result === true) {
+                $("#notification-" + id).html('Truck location hidden.');
+                $("#notification-" + id).removeClass('notification-bad');
+                $("#notification-" + id).addClass('notification-ok');
+            } else {
+                $("#notification-" + id).html('An error occurred during hiding.');
+                $("#notification-" + id).removeClass('notification-ok');
+                $("#notification-" + id).addClass('notification-bad');
+            }
+        }
+    )
+}
+
 
 function loadTruckInformation()
 {
@@ -44,13 +141,26 @@ function loadTruckInformation()
             var contentsToAdd = '';
             json.forEach(function (truck) {
                 contentsToAdd += '<li class="collapsible-container">';
-                contentsToAdd += '<div class="collapsible-header header-full">' + truck.name + "</div>";
+                contentsToAdd += '<div class="collapsible-header header-full center">' + truck.name + "</div>";
                 contentsToAdd += '<div class="collapsible-body">' + getCollapsibleBody(truck) + '</div>';
                 contentsToAdd += '</li>';
             });
 
             $("#truckList").html(contentsToAdd);
             M.updateTextFields();
+
+            $("button[id*=updateButton]").click(function(element) {
+                var id = element.data('truck-id');
+                updateTruckInformation(id);
+            });
+            $("button[id*=updateLocButton]").click(function(element) {
+                var id = element.data('truck-id');
+                updateTruckLocation(id);
+            });
+            $("button[id*=hideButton]").click(function(element) {
+                var id = element.data('truck-id');
+                hideTruckLocation(id);
+            });
         }
     )
 }
